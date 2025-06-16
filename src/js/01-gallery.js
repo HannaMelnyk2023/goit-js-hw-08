833d3d28d1297ce1d303f6078a487788ffc80433
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-06-16 18:43:16 +0300

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+function markup(arr) {
+    return arr
+        .map(
+            ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`)
+        .join("");
+}
+galleryList.insertAdjacentHTML("beforeend", markup(galleryItems));
+
 
 console.log(galleryItems);
