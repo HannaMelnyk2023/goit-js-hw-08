8beb8c6d2be16c48c63806145201fe9907ffaaa9
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-06-19 15:52:28 +0300

    second part complite

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const stopTime = parseFloat(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(function (data) {
+    console.log(data.seconds);
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds)
+}), 1000);
+
+player.setCurrentTime(stopTime).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+
+            break;
+
+        default:
+
+            break;
+    }
+});
