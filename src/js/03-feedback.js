2a6369d255cd02fad1a5397934221c3e798eb18b
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-06-19 18:36:24 +0300

    third part commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const emailField = form.elements.email;
+const messageField = form.elements.message;
+let formData = {
+    email: '',
+    message: '',
+};
+
+form.addEventListener('input', throttle(() => {
+    formData = {
+        email: emailField.value,
+        message: messageField.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}), 500);
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email || '';
+    form.elements.message.value = formData.message || '';
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Form submited:", savedData);
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {
+        email: '',
+        message: '',
+    }
+});
